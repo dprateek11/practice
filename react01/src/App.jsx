ae129e7c6f1743e5c04c27900dd9ff2591d456d2
dprateek11 <dabasprateek11@gmail.com>
2024-08-16 14:22:28 +0530

    simple todo app

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -2,24 +2,43 @@ import {useState} from "react";
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Got to gym from 7-9",
+    completed: false
+  }, {
+    title: "study web development",
+    description: "Study web development",
+    completed: true
+  }])
+
+  function addTodo() {
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc of new todo"
+    }])
+  }
 
   return (
     <div>
-      <CustomButton count={count} setCount={setCount}></CustomButton>
+      <button onClick={addTodo}>Add a random todo</button>
+      {todos.map(function(todo){
+        return <Todo title={todo.title} description={todo.description}></Todo>
+      })}
     </div>
   )
 }
 
-function CustomButton(props) {
-
-  function onClickHandler() {
-    props.setCount(props.count + 1);
-    
-  }
+// todo app {
+// title,
+// description,
+// completed
+//}
+function Todo(props) {
 
   return <div>
-    <button onClick={onClickHandler}>Counter {props.count}</button>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
   </div>
 }
 
