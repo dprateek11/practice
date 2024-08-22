2c1e3dc7fbd5ff940c99f201061dd850ee9c0999
dprateek11 <dabasprateek11@gmail.com>
2024-08-22 09:38:00 +0530

    react-fragment

---
+++ week-6-exp/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+
+  return (
+    <>
+      <Header title="Prateek"></Header>
+      <Header title="Dabas"></Header>
+    </>
+  )
+}
+
+function Header({title}) {
+  return <div>
+    {title}
+  </div>
+}
+
+export default App
