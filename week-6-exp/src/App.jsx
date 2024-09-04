0cd3a44da1415b00c102a25a59fc2b60807d1ac8
dprateek11 <dabasprateek11@gmail.com>
2024-09-04 18:32:52 +0530

    Moving state down

--- week-6-exp/src/App.jsx
+++ week-6-exp/src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+
 function App() {
 
   return (
     <>
-      <Header title="Prateek"></Header>
+      <HeaderWithTitle />
       <Header title="Dabas"></Header>
     </>
   )
+}
+
+function HeaderWithTitle(){
+  const [title, setTitle] = useState("Prateek")
+
+  function changeTitle(){
+    setTitle(Math.random())
+  }
+
+  return <div>
+      <button onClick={changeTitle}>Click to change the title</button>
+      <Header title={title}></Header>
+  </div>
 }
 
 function Header({title}) {
