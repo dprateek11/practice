530914711d13daefac468ca74e59046d173a1b21
dprateek11 <dabasprateek11@gmail.com>
2024-09-04 19:02:25 +0530

    Card Wrapper in react

--- week-6-exp/src/App.jsx
+++ week-6-exp/src/App.jsx
@@ -2,39 +2,22 @@ import { useState } from "react"
 
 function App() {
 
-  let counter = 3;
-
-  const [todos, SetTodos] = useState([{
-    id:1,
-    title: "go to gym",
-    description: "Go to gym in afternoon"
-  },
-  {
-    id:2,
-    title: "go to gym",
-    description: "Go to gym in night"
-  }])
-
-  function addTodo(){
-    SetTodos([...todos, {
-      id: counter++,
-      title: "read",
-      description: "Do reading before bed"
-    }])
-  }
-
   return (
     <>
-      <button onClick={addTodo}>Add a todo!</button>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+      <CarWrapper innerComponent={<TextComponent/>}></CarWrapper>
     </>
   )
 }
 
-function Todo({ title, description}) {
+function CarWrapper({innerComponent}){
+  return <div style={{border:"2px solid black"}}>
+    {innerComponent}
+    </div>
+}
+
+function TextComponent(){
   return <div>
-    <h1>{title}</h1>
-    <h5>{description}</h5>
+      <h1>Hi there!</h1>
   </div>
 }
 
