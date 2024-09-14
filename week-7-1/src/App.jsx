ec7cd9a1c402b96e5e0b3d0a7d13cc83d19d28bb
dprateek11 <dabasprateek11@gmail.com>
2024-09-14 10:17:15 +0530

    Lazy loading

--- week-7-1/src/App.jsx
+++ week-7-1/src/App.jsx
@@ -1,33 +1,30 @@
-
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-import { Dashboard } from './components/Dashboard';
+import { Suspense, lazy } from 'react';
 import { Landing } from './components/Landing';
+const Dashboard = lazy(() => import('./components/Dashboard'));
 
 function App() {
   return (
-    <div>
+    <>
       <BrowserRouter>
         <Appbar />
         <Routes>
-          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/dashboard' element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>}></Route>
           <Route path='/' element={<Landing />}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
-  )
+    </>
+  );
 }
 
 function Appbar() {
-
   const navigate = useNavigate();
-  return <div>
-    <button onClick={() => {
-          navigate("/");
-        }}>Landing</button>
-        <button onClick={() => {
-          navigate("/dashboard");
-        }}>Dashboard</button>
-  </div>
+  return (
+    <div>
+      <button onClick={() => navigate("/")}>Landing</button>
+      <button onClick={() => navigate("/dashboard")}>Dashboard</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
